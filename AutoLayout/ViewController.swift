a0a3b00d6942421a8469aac590be471eaa3b138e
joshuageronimo <joshuag31@hotmail.com>
2017-12-27 13:12:56 -0500

    Set up the bottom controls of the UI using StackViews

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -38,33 +38,80 @@ class ViewController: UIViewController {
         return textView
     }()
     
+    // This Button will act as the previous button
+    let prevButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("PREV", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 13)
+        button.setTitleColor(.gray, for: .normal)
+        return button
+    }()
+    
+    // This UIPageControl will show the user which page they are on
+    let pageControl: UIPageControl = {
+        let pc = UIPageControl()
+        pc.currentPage = 0
+        pc.numberOfPages = 4
+        pc.currentPageIndicatorTintColor = #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)
+        pc.pageIndicatorTintColor = .gray
+        return pc
+    }()
+    
+    // This Button will act as the next button
+    let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("NEXT", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 13)
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Show UI
-        view.addSubview(topImageViewContainer)
-        view.addSubview(descriptionTextView)
-        topImageViewContainer.addSubview(bearImageView)
         setUpLayoutConstraints()
-    }
+        setUpBottomControlsWithStackViews()
+        
+            }
     
     // Contains all the constraints of the views initialized at the top
     private func setUpLayoutConstraints() {
         // MARK: - topImageViewContainer
+        view.addSubview(topImageViewContainer)
         topImageViewContainer.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         topImageViewContainer.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
         topImageViewContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         topImageViewContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
        
         // MARK: - bearImageView-Constraint
+        topImageViewContainer.addSubview(bearImageView)
         bearImageView.centerXAnchor.constraint(equalTo: topImageViewContainer.centerXAnchor).isActive = true
         bearImageView.centerYAnchor.constraint(equalTo: topImageViewContainer.centerYAnchor).isActive = true
         bearImageView.heightAnchor.constraint(equalTo: topImageViewContainer.heightAnchor, multiplier: 0.5).isActive = true
         
         // MARK: - descriptionTextView-Constraint
+        view.addSubview(descriptionTextView)
         descriptionTextView.topAnchor.constraint(equalTo: topImageViewContainer.bottomAnchor).isActive = true
         descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 24).isActive = true
         descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -24).isActive = true
         descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+    // MARK: - Bottom-Controls-StackView
+    fileprivate func setUpBottomControlsWithStackViews() {
+        // StackView Container
+        let bottomContainerStackView = UIStackView(arrangedSubviews: [prevButton, pageControl,  nextButton])
+        view.addSubview(bottomContainerStackView)
+        bottomContainerStackView.distribution = .fillEqually
+        bottomContainerStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Constraints for the StackView
+        NSLayoutConstraint.activate([
+            bottomContainerStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bottomContainerStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            bottomContainerStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            bottomContainerStackView.heightAnchor.constraint(equalToConstant: 50)
+            ])
     }
 }
 
