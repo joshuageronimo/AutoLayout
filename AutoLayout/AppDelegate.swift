8b23ae74945cd531ee7f4fad8e3c2bf2b7e86304
joshuageronimo <joshuag31@hotmail.com>
2017-12-28 22:16:27 -0500

    Create a CollectionView with 4 sections (each sections takes up the whole screen) that scrolls horizontally

--- AutoLayout/AppDelegate.swift
+++ AutoLayout/AppDelegate.swift
@@ -15,7 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // RootView of the app
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let swipingCollectionViewController = SwipingCollectionViewController(collectionViewLayout: layout)
+        window?.rootViewController = swipingCollectionViewController
+        
+        
         return true
     }
 
---
+++ AutoLayout/SwipingCollectionViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SwipingCollectionViewController.swift
+//  AutoLayout
+//
+//  Created by Joshua Geronimo on 12/28/17.
+//  Copyright © 2017 Joshua Geronimo. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class SwipingCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+        collectionView?.isPagingEnabled = true
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 4
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = indexPath.item % 2 == 0 ? .red : .green
+        // Configure the cell
+    
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: view.frame.height)
+    }
+    
+    
+
+}
