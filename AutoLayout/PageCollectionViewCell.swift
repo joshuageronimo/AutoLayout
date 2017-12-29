70528a62541e4ca9487163234e5478fa63e4163c
joshuageronimo <joshuag31@hotmail.com>
2017-12-28 22:45:05 -0500

    Create a custom cell for the CollectionView

---
+++ AutoLayout/PageCollectionViewCell.swift
@@ -0,0 +1,74 @@
+//
+//  PageCollectionViewCell.swift
+//  AutoLayout
+//
+//  Created by Joshua Geronimo on 12/28/17.
+//  Copyright © 2017 Joshua Geronimo. All rights reserved.
+//
+
+import UIKit
+
+class PageCollectionViewCell: UICollectionViewCell {
+    
+    // This UIView contains bearImageView
+    let topImageViewContainer: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    // This UIImageView contains bear_first.png
+    let bearImageView: UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "bear_first"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    // This TextView is used as a UILabel to contain the description
+    let descriptionTextView: UITextView = {
+        let textView = UITextView()
+        let attributedText = NSMutableAttributedString(string: "Join us today in our fun and games!", attributes: [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 18)])
+        attributedText.append(NSAttributedString(string: "\n\n\nAre you ready for loads and loads of fun? Don't wait any longer! We hope to see you in our stores soon.", attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 13), NSAttributedStringKey.foregroundColor: UIColor.gray]))
+        textView.attributedText = attributedText
+        textView.textAlignment = .center
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        // Show UI
+        setUpLayoutConstraints()
+    }
+    
+    // Contains all the constraints of the views initialized at the top
+    private func setUpLayoutConstraints() {
+        // MARK: - topImageViewContainer
+        addSubview(topImageViewContainer)
+        topImageViewContainer.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        topImageViewContainer.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.5).isActive = true
+        topImageViewContainer.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        topImageViewContainer.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        
+        // MARK: - bearImageView-Constraint
+        topImageViewContainer.addSubview(bearImageView)
+        bearImageView.centerXAnchor.constraint(equalTo: topImageViewContainer.centerXAnchor).isActive = true
+        bearImageView.centerYAnchor.constraint(equalTo: topImageViewContainer.centerYAnchor).isActive = true
+        bearImageView.heightAnchor.constraint(equalTo: topImageViewContainer.heightAnchor, multiplier: 0.5).isActive = true
+        
+        // MARK: - descriptionTextView-Constraint
+        addSubview(descriptionTextView)
+        descriptionTextView.topAnchor.constraint(equalTo: topImageViewContainer.bottomAnchor).isActive = true
+        descriptionTextView.leftAnchor.constraint(equalTo: leftAnchor, constant: 24).isActive = true
+        descriptionTextView.rightAnchor.constraint(equalTo: rightAnchor, constant: -24).isActive = true
+        descriptionTextView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
--- AutoLayout/SwipingCollectionViewController.swift
+++ AutoLayout/SwipingCollectionViewController.swift
@@ -14,34 +14,38 @@ class SwipingCollectionViewController: UICollectionViewController, UICollectionV
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
+        // Register PageCollectionViewCell
+        self.collectionView!.register(PageCollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        // Sets the background to white
+        collectionView?.backgroundColor = .white
+        // This code will do the snapping effect on each scroll between pages
         collectionView?.isPagingEnabled = true
     }
 
     // MARK: UICollectionViewDataSource
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
+        // there will be four pages
         return 4
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-        cell.backgroundColor = indexPath.item % 2 == 0 ? .red : .green
         // Configure the cell
-    
         return cell
     }
     
+    // MARK: UICollectionViewDelegateFlowLayout
+    
+    // This function dictates the size of each cell.
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // Make each cell take up the whole screen.
         return CGSize(width: view.frame.width, height: view.frame.height)
+    }
+    
+    // This function dictates the amount of spaces between each cell.
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0    // no space
     }
     
     
